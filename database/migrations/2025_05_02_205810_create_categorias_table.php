28f6949aa662a33093aad4e7e4b42d6669135d63
Faustino Vasquez <vasquez.limon@gmail.com>
2025-05-05 15:58:32 -0700

    categorias

--- database/migrations/2025_05_02_205810_create_categorias_table.php
+++ database/migrations/2025_05_02_205810_create_categorias_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->enum('type', ['ingreso', 'gasto']);
+            $table->string('nombre');
+            $table->enum('tipo', ['ingreso', 'gasto']);
             $table->timestamps();
         });
     }
